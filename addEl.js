7c8828b43dc712bfd36af328e7f52a13ca3f4ee3
karenm09 <karenm929@hotmail.com>
2019-11-10 17:25:40 -0500

    startup

---
+++ addEl.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let button = document.querySelector("#btn")
+    button.addEventListener("click", swapHeader())
+
+})
+const swapHeader = () => {
+    let oldHead = document.querySelector("h1")
+    let currentHead = document.createElement("h1")
+    let input = document.querySelector("#input").value
+    currentHead.innerText = input
+    oldHead.parentNode.replaceChild(currentHead, oldHead)
+}
