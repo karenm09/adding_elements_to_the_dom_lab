e12ab07627f379645570eeb57490614f2c6aa2ce
Samantha Jimenez <samanthajimenez@pursuit.org>
2019-11-11 20:21:44 -0500

    complete problem 2

--- addEl.js
+++ addEl.js
@@ -1,12 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
     let button = document.querySelector("#btn")
-    button.addEventListener("click", swapHeader())
+    button.addEventListener("click", question1)
+
+    question2();
+
 
 })
-const swapHeader = () => {
+const question2 = () => {
+    let ul = document.createElement("ul");
+    
+    let object = {
+        "results" : [
+          { "name" : "Anne" },
+          { "name" : "Bri" },
+          { "name" : "Chaz" },
+          { "name" : "Darlene" }
+        ]
+      };
+      for(let n of object.results) {
+        let nameList = document.createElement("li")
+        nameList.innerText = n.name;
+        ul.appendChild(nameList);
+      }
+      document.body.appendChild(ul);
+      console.log(ul);
+}
+
+const question1 = () => {
     let oldHead = document.querySelector("h1")
     let currentHead = document.createElement("h1")
     let input = document.querySelector("#input").value
     currentHead.innerText = input
-    oldHead.parentNode.replaceChild(currentHead, oldHead)
+    oldHead.parentNode.replaceChild(currentHead, oldHead);
 }
